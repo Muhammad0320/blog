37607668f2798a4b4de6735e18119167255853fb
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2025-10-10 11:07:01 +0100

    chore: add db conn

--- server/src/data/db.ts
+++ server/src/data/db.ts
@@ -1,5 +1,27 @@
 import mysql from "mysql2/promise";
+import dotenv from "dotenv";
 
-const pool = mysql.createPool({});
+dotenv.config();
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+async function checkConnection() {
+  try {
+    const conn = await pool.getConnection();
+    console.log("✅ Database connected successfully ");
+  } catch (error) {
+    console.error("❌ Could not connect to database:", error);
+  }
+}
+
+checkConnection();
 
 export default pool;
