822773967e9a8dd7a4141ed62bef0bacd0544a28
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2025-10-12 04:06:20 +0100

    chore: add comment modle

--- blog_db.session.sql
+++ blog_db.session.sql
@@ -3,4 +3,14 @@ CREATE TABLE posts (
     title VARCHAR(255) NOT NULL,
     content TEXT NOT NULL, 
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
-)
+);
+
+CREATE TABLE comments (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  content TEXT NOT NULL,
+  author VARCHAR(255),
+  post_id INT NOT NULL, 
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE
+);
+
