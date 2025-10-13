3a60f1bccf133256385f92d96d57e2c7b878607b
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2025-10-13 04:05:15 +0100

    chore: add create controller

--- server/src/data/controller/comment,controller.ts
+++ server/src/data/controller/comment,controller.ts
@@ -18,4 +18,31 @@ class CommentController {
       throw new Error("Can not get all comments");
     }
   }
+
+  static async handleCreate(req: Request, res: Response) {
+    try {
+      const { content, postId, author } = req.body;
+
+      if (!content || !postId || !author) {
+        res
+          .status(400)
+          .json({ message: "content, postId and author field are requireed" });
+      }
+
+      const affectedRow = await CommentModel.create({
+        content,
+        postId,
+        author,
+      });
+      // Is this the right way of checking for a postid not found
+      if (affectedRow < 1) {
+        res.status(404).json({ message: "Post id not found!" });
+      }
+
+      res.json(201).json({ message: "Comment successfully created!" });
+    } catch (error) {
+      console.error("Contoller error: Error creating new comment", error);
+      throw new Error("Cannot create comment");
+    }
+  }
 }
