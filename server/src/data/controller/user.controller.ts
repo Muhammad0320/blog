ce8b387891e4f474c0e0c9bb83c9707e809e5754
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2025-10-15 10:00:04 +0100

    chore: update user controller

--- server/src/data/controller/user.controller.ts
+++ server/src/data/controller/user.controller.ts
@@ -1,24 +1,37 @@
 import { Request, Response } from "express";
 import { UserModel } from "../models/user.model";
-import { report } from "process";
+import { createUserSchema } from "../../lib/validators";
+import z, { ZodError } from "zod";
 
 export class UserController {
   static async handleCreate(req: Request, res: Response) {
     try {
-      const { username, email, password } = req.body;
-      // I think it high time to have a validator library instead of doing this manually!
-      if (!username || !email || !password) {
-        res.status(400).json({ message: "All input fields are required" });
+      const userData = createUserSchema.parse(req.body);
+      const userId = await UserModel.create(userData);
+      res.status(201).json({ userId, message: "User successfully created" });
+    } catch (error) {
+      if (error instanceof ZodError) {
+        res
+          .status(400)
+          .json({
+            message: "Invalid input",
+            errors: z.treeifyError(error).errors,
+          });
         return;
       }
 
-      const userId = await UserModel.create({ username, email, password });
-      res
-        .status(201)
-        .json({ user_id: userId, message: "User successfully created" });
-    } catch (error) {
-      console.error("Contoller error: cannot create user", error);
+      if (
+        error &&
+        typeof error === "object" &&
+        "message" in error &&
+        typeof error.message == "string"
+      ) {
+        if (error.message.includes("already exists")) {
+          return res.status(409).json({ message: error.message });
+        }
+      }
 
+      console.error("Controller error: Could not create user", error);
       res.status(500).json({ message: "Error creating user" });
     }
   }
