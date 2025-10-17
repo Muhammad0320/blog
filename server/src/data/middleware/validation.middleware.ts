7f5676e58e6e54614a703c1b3d8ef128906b4903
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2025-10-17 07:54:06 +0100

    feate: create a validation factory

--- server/src/data/middleware/validation.middleware.ts
+++ server/src/data/middleware/validation.middleware.ts
@@ -1,62 +1,20 @@
 import { Request, Response, NextFunction } from "express";
 
-export const validatePostId = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  const { postId } = req.params;
-  const id = parseInt(postId, 10);
+export const validateId = (paramName: string, propertyName: keyof Request) => {
 
-  if (isNaN(id)) {
-    return res
-      .status(400)
-      .json({ message: "Invalid post ID. Must be a number." });
+  return (req: Request, res: Response, next: NextFunction) => {
+
+    const idParam = req.params[paramName]
+    const id = parseInt(idParam, 10)
+    if (isNaN(id)) {
+      return res.status(400).json({message: `Invalid ${paramName}. Must be a number`})
+    }
+
+    (req as any)[propertyName] = id 
+    next()
   }
+}
 
-  req.postId = id;
-  next();
-};
-
-/**
- * NEW: Middleware to validate the comment ID from the URL parameters.
- */
-export const validateCommentId = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  // We look for 'commentId' in the URL
-  const { commentId } = req.params;
-  const id = parseInt(commentId, 10);
-
-  if (isNaN(id)) {
-    return res
-      .status(400)
-      .json({ message: "Invalid comment ID. Must be a number." });
-  }
-
-  // We attach it to the request as 'req.commentId'
-  req.commentId = id;
-  next();
-};
-
-export const validateUserId = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  // We look for 'commentId' in the URL
-  const { userId } = req.params;
-  const id = parseInt(userId, 10);
-
-  if (isNaN(id)) {
-    return res
-      .status(400)
-      .json({ message: "Invalid user ID. Must be a number." });
-  }
-
-  // We attach it to the request as 'req.userId'
-  req.userId = id;
-  next();
-};
+export const validatePostId = validateId("postId", "postId")
+export const validateCommentId = validateId("postId", "postId")
+export const validateUserId = validateId("postId", "postId")
