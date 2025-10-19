581c73291482c6b9324be3a9acbd61cb49ced523
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2025-10-19 11:08:44 +0100

    chore: update comment model

--- server/src/data/models/comment.model.ts
+++ server/src/data/models/comment.model.ts
@@ -8,21 +8,24 @@ export { Comment };
 interface NewCommentData {
   content: string;
   postId: number;
-  author?: string;
+  userId?: number;
 }
 
 export class CommentModel {
   static async getAllForPost(postId: number): Promise<Comment[]> {
-    return prisma.comments.findMany({ where: { post_id: postId } });
+    return prisma.comments.findMany({
+      where: { post_id: postId },
+      include: { author: { select: { id: true, username: true } } },
+    });
   }
 
   static async create(data: NewCommentData): Promise<Comment> {
-    const { content, postId, author } = data;
+    const { content, postId, userId } = data;
 
     return prisma.comments.create({
       data: {
         content,
-        author,
+        author: { connect: { id: userId } },
         posts: {
           connect: { id: postId },
         },
