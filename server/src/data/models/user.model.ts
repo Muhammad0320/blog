1b69b8e5c412100d0f5d4c16c9bafe8685639ef5
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2025-10-19 11:30:02 +0100

    chore: update code

--- server/src/data/models/user.model.ts
+++ server/src/data/models/user.model.ts
@@ -3,15 +3,10 @@ import pool from "../db";
 import { z } from "zod";
 import { userCreateSchema } from "../../lib/validators";
 import { Password } from "../../lib/auth";
+import { users as User } from "../../generated/prisma";
+import prisma from "../../db/prisma";
 
-export interface User extends RowDataPacket {
-  id: number;
-  username: string;
-  email: string;
-  password: string; // I think it will be in bytes or hex when hashes
-  created_at: Date;
-  role: "user" | "admin";
-}
+export { User };
 
 export type PublicUser = Omit<User, "password">;
 
@@ -20,68 +15,36 @@ export type UserData = z.infer<typeof userCreateSchema>;
 export type UserProfileInfo = {
   id: number;
   username: string;
-  created_at: Date;
+  created_at: Date | null;
 };
 
 export class UserModel {
-  static async create({
-    username,
-    email,
-    password,
-  }: UserData): Promise<number> {
-    // Check for duplicate username/email
-    const checkSql = `SELECT id FROM users WHERE username = ? OR email = ?`;
-    const [existingUser] = await pool.query<User[]>(checkSql, [
-      username,
-      email,
-    ]);
+  static async create({ username, email, password }: UserData): Promise<User> {
+    const existingUser = await prisma.users.findFirst({
+      where: { OR: [{ username }, { email }] },
+    });
 
-    if (existingUser.length > 0) {
-      throw new Error("Username or email already exists.");
+    if (existingUser) {
+      throw new Error("Username or email already exists");
     }
 
-    // Hash the password
     const hashedPassword = await Password.hash(password);
 
-    const insertSql = `INSERT INTO users (username, email, password) VALUES (?, ?, ?)`;
-    const values = [username, email, hashedPassword];
-    try {
-      const [result] = await pool.query<ResultSetHeader>(insertSql, values);
-
-      return result.insertId;
-    } catch (error) {
-      console.error("Error creating user", error);
-      throw new Error("Cannot create user");
-    }
+    return prisma.users.create({
+      data: { username, email, password: hashedPassword },
+    });
   }
 
   static async findByEmail(email: string): Promise<User | null> {
-    const sql = `SELECT * FROM users WHERE email = ?`;
-    try {
-      const [rows] = await pool.query<User[]>(sql, [email]);
-
-      return rows[0] || null;
-    } catch (error) {
-      console.error("DB Error: could not find user with such email", email);
-      throw new Error("Error finding user");
-    }
+    return prisma.users.findUnique({ where: { email } });
   }
 
   static async findPublicProfileById(
     id: number
   ): Promise<UserProfileInfo | null> {
-    const sql = `SELECT id, username, created_at FROM users WHERE id = ?`;
-
-    try {
-      const [rows] = await pool.query<(UserProfileInfo & RowDataPacket)[]>(
-        sql,
-        [id]
-      );
-
-      return rows[0] || null;
-    } catch (error) {
-      console.error("DB error: could not find user with such id ", error);
-      throw new Error("Error finding user profile");
-    }
+    return prisma.users.findUnique({
+      where: { id },
+      select: { id: true, username: true, created_at: true },
+    });
   }
 }
